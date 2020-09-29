179a3741ae886dd43d26d5a7037a4128c577ac79
MichaelOmondi <omondimike11@gmail.com>
2020-09-29 16:13:17 +0300

    import mail

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_mail import Mail
-from config import config_options.
+from config import config_options
 
 # from config import Config
 
